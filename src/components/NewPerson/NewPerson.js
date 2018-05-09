5108252a42d333d5d197facd4d208d3d31952471
Mhiho <m.pelka7@gmail.com>
2018-05-09 13:08:18 +0200

    enough

--- src/components/NewPerson/NewPerson.js
+++ src/components/NewPerson/NewPerson.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import './NewPerson.css';
 
 const NewPerson = (props) => (
   <div onClick={props.click}>
-  <h1>Is {props.name} a SUPERHERO? ? ???</h1>
+  <h1>Is {props.name} a superhero ? </h1>
   <h2>He is {props.age} years old</h2>
-  <h1>{props.animal}</h1>
+  <h1 className="Super">{props.animal}</h1>
   </div>
 );
 
